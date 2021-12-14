75312acbe06f50cbe1ef5ecc2ffa2f4e0cfc87c6
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:50:40 +0530

    Create Armstrong.java

---
+++ Cycle1/Armstrong.java
@@ -0,0 +1,40 @@
+//Java program to check whether a number is armstrong or not.
+
+import java.util.*;
+import java.lang.*;
+
+class Armstrong
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int num,digit=0,sum=0,last=0;
+		
+		System.out.println("Enter number:");
+		num=sc.nextInt();
+		
+		int a=num;
+		while(a!=0)
+		{
+			a/=10;
+			digit++;
+		}
+		a=num;
+		while(a!=0)
+		{
+			last=a%10;
+			sum+=Math.pow(last,digit);
+			a/=10;
+		}
+		
+		if(sum==num)
+		{
+			System.out.println("Armstrong");
+		}
+		else
+		{
+			System.out.println("Not Armstrong");
+		}
+	}
+}
