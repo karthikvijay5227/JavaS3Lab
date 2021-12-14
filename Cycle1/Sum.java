06306852fe5c319fc9ba62d73e1fd7598eb880d9
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:05:58 +0530

    Create Sum.java

---
+++ Cycle1/Sum.java
@@ -0,0 +1,23 @@
+//Java prgram to add two numbers.
+
+import java.util.*;
+class Sum
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+    int a,b,sum=0;
+    
+    System.out.println("Enter first number:");
+    a=sc.nextInt();
+    
+    System.out.println("Enter second number:");
+    b=sc.nextInt();
+    
+    sum=a+b;
+    
+    System.out.println("The sum is:"+sum);
+    
+  }
+}
