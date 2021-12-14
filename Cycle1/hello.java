cb45602d87326886f937af448fea74b7f8c17770
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:02:48 +0530

    Update hello.java

--- Cycle1/hello.java
+++ Cycle1/hello.java
@@ -1,7 +1,9 @@
+//Java Program to print Hello World.
+
 class hello
 {
   public static void main(String args[])
   {
-    System.out.println("Hello");
+    System.out.println("Hello World");
   }
 }
