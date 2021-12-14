ef40800d3567b94d61e5895876768b9f23ee5e98
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:25:27 +0530

    Create Factor.java

---
+++ Cycle1/Factor.java
@@ -0,0 +1,23 @@
+//Java program to print factor of given number
+
+import java.util.*;
+
+class factor
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int n;
+		System.out.println("Enter the number:");
+		n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
