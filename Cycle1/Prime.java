d37a9395e0221639ef97c4ee2986cbc32f5f6e1c
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:16:53 +0530

    Create Prime.java

---
+++ Cycle1/Prime.java
@@ -0,0 +1,33 @@
+//Java program to check whether a number is prime or not.
+
+import java.util.*;
+
+class Prime
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+    int a,flag=0;
+    System.out.println("Enter the number:");
+    a=sc.nextInt();
+    
+    for(int i=2;i<=a/2;i++)
+    {
+      if(a%i==0)
+      {
+        flag=1;
+        break;
+      }
+		}
+		
+		if(flag==0)
+		{
+			System.out.println(a+" is prime");
+		}
+		else
+		{
+			System.out.println(a+" is not prime");
+		}
+	}
+}
