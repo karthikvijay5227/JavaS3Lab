dfca4da0c31a81ccbf3b24744abc67b4ec519bde
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:39:58 +0530

    Create Factorial.java

---
+++ Cycle1/Factorial.java
@@ -0,0 +1,24 @@
+//Java program to print factorial of first n numbers.
+
+import java.util.*;
+
+class Factorial
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int n,fact=1;
+		
+		System.out.println("Enter the number:");
+		n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++)
+		{
+			fact*=i;
+		}
+		
+		System.out.println("The factorial is:"+fact);
+		
+	}
+}
