d68b503da232de1cbf744a828a499f9219abd6e6
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:32:22 +0530

    Create Fibonacci.java

---
+++ Cycle1/Fibonacci.java
@@ -0,0 +1,26 @@
+//Java program to print Fibonacci series.
+
+import java.util.*;
+
+class Fibonacci
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+    int first=0,second=1,third,n;
+    System.out.println("Enter the number:");
+    n=sc.nextInt();
+    
+    System.out.print("Fibonacci series"+first+" "+second);
+    
+    for(int i=2;i<=n;i++)
+    {
+      third=first+second;
+      System.out.print(" "+third+" ");
+      first=second;
+      second=third;
+    }
+	}
+}
+    
