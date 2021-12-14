829635baa9d5dc83e061133e8db0432036184403
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:21:13 +0530

    Create NaturalSum.java

---
+++ Cycle1/NaturalSum.java
@@ -0,0 +1,23 @@
+//Java program to print the sum of first n natural numbers.
+
+import java.util.*;
+
+class NaturalSum
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+    int n,sum=0;
+    System.out.println("Enter the number:");
+    n=sc.nextInt();
+    
+    for(int i=1;i<=n;i++)
+    {
+      sum+=i;
+    }
+		
+    System.out.println("The sum of first "+n+" natural numbers is:"+sum);
+    
+  }
+}
