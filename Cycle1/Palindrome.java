49bea2ac0d1b0a8cac16d48d87dcba51311d20b0
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-12-14 20:45:36 +0530

    Create Palindrome.java

---
+++ Cycle1/Palindrome.java
@@ -0,0 +1,33 @@
+//Java program to check whether a number is palindrome or not.
+
+import java.util.*;
+
+class Palindrome
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int num,digit,rev=0;
+		
+		System.out.println("Enter number:");
+		num=sc.nextInt();
+		
+		int a=num;
+		while(a!=0)
+		{
+			digit=a%10;
+			rev=(rev*10)+digit;
+			a/=10;
+		}
+		
+		if(rev==num)
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+	}
+}
