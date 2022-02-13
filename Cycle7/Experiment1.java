962b557d37903bc63b8796e6ffcfd496995e8bae
Karthik Vijay <kv52274@gmail.com>
2022-02-13 13:53:53 +0530

    Update and rename ThreadingEg.java to Experiment1.java

---
+++ Cycle7/Experiment1.java
@@ -0,0 +1,73 @@
+import java.util.*;
+class Square extends Thread
+{
+	int n;	
+	Square(int n)
+	{
+		this.n=n;
+	}	
+    public void run()
+  	{
+  	  int a=this.n*this.n;	
+		System.out.println("Square is:"+a);
+  	} 
+}
+class Cube extends Thread
+{
+	int n;
+	Cube(int n)
+	{
+		this.n=n;
+	}
+	public void run()
+	{
+		int b=this.n*this.n*this.n;
+		System.out.println("Cube is:"+b);
+	}
+}
+
+class Calc extends Thread
+{
+	public void run()
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int n;
+	  System.out.print("Enter no of times to do:"); 
+	  n=sc.nextInt();
+	  System.out.print("Enter the range of random numbers:");
+	  int range=sc.nextInt();
+	  Random r=new Random();
+	  for(int I=0;I<n;I++)
+	  {
+   	int x=r.nextInt(range);
+   	System.out.println();
+ 	 System.out.println("The random number is:"+x);
+   	if(x%2==0)
+  	{
+		Square s=new Square(x);
+		s.start();
+  	}
+ 	else
+      {
+    	Cube cu=new Cube(x);
+    	cu.start();
+      }
+    try
+    {
+      Thread.sleep(1000);
+    }
+    catch(Exception e)
+    {
+      System.out.println(e);
+    }
+   }     
+}
+}	
+ public class Experiment1
+ {
+	public static void main(String[] args)throws Exception 
+	{
+		Calc c=new Calc();
+		c.start();
+	}
+}
