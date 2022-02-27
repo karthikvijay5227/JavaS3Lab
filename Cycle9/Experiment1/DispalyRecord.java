572f4da84197045411e27a740e7e679d1b5e1f03
Karthik Vijay <kv52274@gmail.com>
2022-02-27 14:34:50 +0000

    Create DispalyRecord.java

---
+++ Cycle9/Experiment1/DispalyRecord.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+class DisplayRecord
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306","karthik","karthik123");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM RECORD");
+            System.out.println("No \t Name \t Age \t Place" );
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t\t"+rs.getInt(3)+"\t\t"+rs.getString(4));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
